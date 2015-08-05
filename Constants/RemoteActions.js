d4032e08c69bef0ff8b1c3ccd975358a831f058a
emmadiharish <harishemmadi@Harishs-MacBook-Pro.local>
2015-08-05 11:00:05 -0700

    test

--- Constants/RemoteActions.js
+++ Constants/RemoteActions.js
@@ -1,4 +1,5 @@
 angular.module('APTPS_ngCPQ').constant('RemoteActions',{
+	getLineItemtest: 'APTPS_CAP_RA.getLineItem',
 	getLineItem: 'APTPS_CAP_RA.getLineItem',
 	getServiceLocations: 'APTPS_CAP_RA.getServiceLocations',
 	getPricingMatrixData: 'APTPS_CAP_RA.getpricingMatrix',
