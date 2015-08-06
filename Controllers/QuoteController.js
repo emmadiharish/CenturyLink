d48497971a2d1b13d01685997f73ea693b1d02a7
emmadiharish <hemmadi@apttus.com>
2015-08-06 11:05:14 -0700

    quotecontroller

---
+++ Controllers/QuoteController.js
@@ -0,0 +1,18 @@
+(function() {
+    var QuoteController;
+
+    QuoteController = function($scope, QuoteDataService) {
+    	$scope.init = function(){
+    		$scope.lineItem = QuoteDataService.getlineItem();
+    		$scope.QuoteId = $scope.lineItem.Apttus_Config2__ConfigurationId__r.Apttus_QPConfig__Proposald__c;
+    		$scope.QuoteName = $scope.lineItem.Apttus_Config2__ConfigurationId__r.Apttus_QPConfig__Proposald__r.Apttus_Proposal__Proposal_Name__c;
+        	$scope.QuoteNumber = $scope.lineItem.QuoteApttus_Config2__ConfigurationId__r.Apttus_QPConfig__Proposald__r.Name;
+        	$scope.ApprovalStatus = $scope.lineItem.Apttus_Config2__ConfigurationId__r.Apttus_QPConfig__Proposald__r.Apttus_QPApprov__Approval_Status__c;
+        }
+    	
+    	$scope.init();
+    };
+
+    QuoteController.$inject = ['$scope', 'QuoteDataService'];
+    angular.module('APTPS_ngCPQ').controller('QuoteController', QuoteController);
+}).call(this);
