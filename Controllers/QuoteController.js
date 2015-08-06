e682604ec71ed5729c385a721b812c888a122898
emmadiharish <hemmadi@apttus.com>
2015-08-06 11:20:37 -0700

    quotecont 3

--- Controllers/QuoteController.js
+++ Controllers/QuoteController.js
@@ -7,7 +7,7 @@
     		$scope.QuoteId = $scope.lineItem.Apttus_Config2__ConfigurationId__r.Apttus_QPConfig__Proposald__c;
     		$scope.QuoteName = $scope.lineItem.Apttus_Config2__ConfigurationId__r.Apttus_QPConfig__Proposald__r.Apttus_Proposal__Proposal_Name__c;
         	$scope.QuoteNumber = $scope.lineItem.Apttus_Config2__ConfigurationId__r.Apttus_QPConfig__Proposald__r.Name;
-        	$scope.ApprovalStatus = $scope.lineItem.Apttus_Config2__ConfigurationId__r.Apttus_QPConfig__Proposald__r.Apttus_QPApprov__Approval_Status__c;
+        	$scope.ApprovalStatus = QuoteDataService.getApprovalStatus();
         }
     	
     	$scope.init();
