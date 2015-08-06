fb70b93ae9b2ba61b40740bdd9a9e632e5c8e257
emmadiharish <hkumar@centerstance.com>
2015-08-06 12:20:02 -0700

    Update and rename reloadconfirm.js to reloadConfirm.js

--- js/reloadconfirm.js
+++
@@ -1,8 +0,0 @@
-var myEvent = window.attachEvent || window.addEventListener;
-var chkevent = window.attachEvent ? 'onbeforeunload' : 'beforeunload'; /// make IE7, IE8 compatible
-
-myEvent(chkevent, function(e) { // For >=IE7, Chrome, Firefox
-    var confirmationMessage = ' ';  // a space
-    (e || window.event).returnValue = confirmationMessage;
-    return confirmationMessage;
-});
