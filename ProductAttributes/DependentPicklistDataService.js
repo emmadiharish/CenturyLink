a020c4f4e833b2438481aa7cdc0b211ea1fff93f
emmadiharish <hemmadi@apttus.com>
2015-08-12 16:42:36 -0700

    dependecy change

--- ProductAttributes/DependentPicklistDataService.js
+++ ProductAttributes/DependentPicklistDataService.js
@@ -5,6 +5,7 @@
 		var service = this;
 
 		service.isValid = false;
+		service.PAVcFieldtodFieldDefinationMap = {};
 		service.PAVcFieldtodFieldssMap = {};
 		service.PAVFieldDOptionsMap = {};
 
@@ -39,20 +40,36 @@
             	dependentFields.push(dField);
             	res[cField] = dependentFields;
             	service.PAVFieldDOptionsMap[cField+dField] = dpwrapper.objResult;
+            	
+            	var dFieldDefination[dField] = dpwrapper.objResult;
+            	var dFieldDefinationList = [];
+            	if(_.has(service.PAVcFieldtodFieldDefinationMap, cField))
+            	{
+            		dFieldDefinationList = service.PAVcFieldtodFieldDefinationMap[cField];
+            	}
+            	dFieldDefinationList.push(dFieldDefination);
+            	service.PAVcFieldtodFieldDefinationMap[cField] = dFieldDefinationList;
             });
+
             service.PAVcFieldtodFieldssMap = res;
 		}
 
 		function getStructuredDependentFields(cField){
+			var res = [];
+			if(_.has(service.PAVcFieldtodFieldDefinationMap, cField))
+			{
+				res = service.PAVcFieldtodFieldDefinationMap[cField];
+			}
+			return res;	
+		}
+
+		function getStructuredDependentFields_old(cField){
 			var res = {};
 			if(_.has(service.PAVcFieldtodFieldssMap, cField))
 			{
-				var dFields = [];
-				_.each(service.PAVcFieldtodFieldssMap[cField], function(fields){
-					dFields.push(fields);
-				});
-				dFields = _.flatten(dFields);
-				res['dFields'] = dFields;
+				var dFields = getListOfDependentFieldsfor(cField);
+
+				res[cField+'DependentFieldsList'] = dFields;
 
 				_.each(dFields, function(dField){
 					var fieldcombination = cField+dField;
@@ -61,6 +78,18 @@
 						res[fieldcombination] = service.PAVFieldDOptionsMap[fieldcombination];
 					}
 				});
+			}
+			return res;	
+		}
+
+		function getListOfDependentFieldsfor(cField){
+			var res = [];
+			if(_.has(service.PAVcFieldtodFieldssMap, cField))
+			{
+				_.each(service.PAVcFieldtodFieldssMap[cField], function(fields){
+					res.push(fields);
+				});
+				res = _.flatten(res);
 			}
 			return res;	
 		}
