b0de046c41a411c6aeb06b5ecbbf17064a54df02
emmadiharish <harishemmadi@Harishs-MacBook-Pro.local>
2015-08-27 17:24:50 -0700

    akjdba

--- PricingMatrix/PricingMatrixDataService.js
+++ PricingMatrix/PricingMatrixDataService.js
@@ -1,7 +1,7 @@
 (function() {
 	angular.module('APTPS_ngCPQ').service('PricingMatrixDataService', PricingMatrixDataService); 
-	PricingMatrixDataService.$inject = ['$q', '$log', 'QuoteDataService', 'RemoteService'];
-	function PricingMatrixDataService($q, $log, QuoteDataService, RemoteService) {
+	PricingMatrixDataService.$inject = ['$q', '$log', 'BaseService', 'QuoteDataService', 'RemoteService'];
+	function PricingMatrixDataService($q, $log, BaseService, QuoteDataService, RemoteService) {
 		var service = this;
 
 		service.pricingMatrixSearchRes = {};
@@ -17,9 +17,11 @@
 			}
 			
 			var requestPromise = RemoteService.getPricingMatrixData(QuoteDataService.getbundleproductId());
+			BaseService.startprogress();// start progress bar.
 			return requestPromise.then(function(response){
 				service.isValid = true;
 				service.pricingMatrixSearchRes = response;
+				BaseService.setLocationLoadComplete();
 				// logTransaction(response, categoryRequest);
 				return service.pricingMatrixSearchRes;
 			});
