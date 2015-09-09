de3aed61596da9755b6b4316ad2803ec15009cbe
emmadiharish <hemmadi@apttus.com>
2015-09-09 14:19:30 -0700

    asdasdas

--- MiniCart/MiniCartDirective.js
+++ MiniCart/MiniCartDirective.js
@@ -9,21 +9,21 @@
 	MiniCartCtrl.$inject = ['$window', '$dialogs', 'SystemConstants', 'QuoteDataService', 'BaseService', 'MiniCartDataService'];
 	
 	function MiniCartCtrl($window, $dialogs, SystemConstants, QuoteDataService, BaseService, MiniCartDataService){
-		var miniCartCtrl = this;
-		miniCartCtrl.itemsPerPage = 5;
-        miniCartCtrl.pagedItems = [];
-        miniCartCtrl.currentPage = 0;
-        miniCartCtrl.lineCount = 0;
+		var cartCtrl = this;
+		cartCtrl.itemsPerPage = 5;
+        cartCtrl.pagedItems = [];
+        cartCtrl.currentPage = 0;
+        cartCtrl.lineCount = 0;
 
 		function init(){
             // Initialize Scope Variables
-            miniCartCtrl.miniCartService = MiniCartDataService;
-            miniCartCtrl.quoteService = QuoteDataService;
-            miniCartCtrl.baseService = BaseService;
+            cartCtrl.miniCartService = MiniCartDataService;
+            cartCtrl.quoteService = QuoteDataService;
+            cartCtrl.baseService = BaseService;
             
-            miniCartCtrl.miniCartTemplateURL = SystemConstants.baseUrl+'/Templates/MiniCartView.html';
-            miniCartCtrl.paginationLinksTemplateURL = SystemConstants.baseUrl+'/Templates/PaginationLinksView.html';
-            miniCartCtrl.imagesbaseURL = SystemConstants.baseUrl+'/Images';
+            cartCtrl.miniCartTemplateURL = SystemConstants.baseUrl+'/Templates/MiniCartView.html';
+            cartCtrl.paginationLinksTemplateURL = SystemConstants.baseUrl+'/Templates/PaginationLinksView.html';
+            cartCtrl.imagesbaseURL = SystemConstants.baseUrl+'/Images';
             
             // Group by pages
             groupToPages();
@@ -31,47 +31,47 @@
 
         // Calculate Total Number of Pages based on Records Queried 
         function groupToPages() {
-            miniCartCtrl.currentPage = 0;
-            miniCartCtrl.miniCartService.getMiniCartLines().then(function(result) {
-                miniCartCtrl.items = result;        
-                miniCartCtrl.lineCount = miniCartCtrl.items.length;
-                miniCartCtrl.pagedItems = [];
-                for (var i = 0; i < miniCartCtrl.items.length; i++) {
-                    if (i % miniCartCtrl.itemsPerPage === 0) {
-                        miniCartCtrl.pagedItems[Math.floor(i / miniCartCtrl.itemsPerPage)] = [miniCartCtrl.items[i]];
+            cartCtrl.currentPage = 0;
+            cartCtrl.miniCartService.getMiniCartLines().then(function(result) {
+                cartCtrl.items = result;        
+                cartCtrl.lineCount = cartCtrl.items.length;
+                cartCtrl.pagedItems = [];
+                for (var i = 0; i < cartCtrl.items.length; i++) {
+                    if (i % cartCtrl.itemsPerPage === 0) {
+                        cartCtrl.pagedItems[Math.floor(i / cartCtrl.itemsPerPage)] = [cartCtrl.items[i]];
                     } else {
-                        miniCartCtrl.pagedItems[Math.floor(i / miniCartCtrl.itemsPerPage)].push(miniCartCtrl.items[i]);
+                        cartCtrl.pagedItems[Math.floor(i / cartCtrl.itemsPerPage)].push(cartCtrl.items[i]);
                     }
                 }
             })
         };
             
-        miniCartCtrl.firstPage = function () {
-            miniCartCtrl.currentPage = 0;
+        cartCtrl.firstPage = function () {
+            cartCtrl.currentPage = 0;
         };
         
-        miniCartCtrl.lastPage = function () {
-            miniCartCtrl.currentPage = miniCartCtrl.pagedItems.length-1;
+        cartCtrl.lastPage = function () {
+            cartCtrl.currentPage = cartCtrl.pagedItems.length-1;
         };
         
-        miniCartCtrl.prevPage = function () {
-            if (miniCartCtrl.currentPage > 0) {
-                miniCartCtrl.currentPage--;
+        cartCtrl.prevPage = function () {
+            if (cartCtrl.currentPage > 0) {
+                cartCtrl.currentPage--;
             }
         };
         
-        miniCartCtrl.nextPage = function () {
-            if (miniCartCtrl.currentPage < miniCartCtrl.pagedItems.length - 1) {
-                miniCartCtrl.currentPage++;
+        cartCtrl.nextPage = function () {
+            if (cartCtrl.currentPage < cartCtrl.pagedItems.length - 1) {
+                cartCtrl.currentPage++;
             }
         };
         
-        miniCartCtrl.setPage = function () {
-            miniCartCtrl.currentPage = this.n;
+        cartCtrl.setPage = function () {
+            cartCtrl.currentPage = this.n;
         };
         
-        miniCartCtrl.invokeDoConfigure = function(lineItemId){
-            miniCartCtrl.miniCartService.configureLineItem(lineItemId).then(function(result){
+        cartCtrl.invokeDoConfigure = function(lineItemId){
+            cartCtrl.miniCartService.configureLineItem(lineItemId).then(function(result){
                 // redirect the page to config URL.
                 var configUrl = parsePagereference(result);
                 if(!_.isNull(configUrl))
@@ -80,26 +80,26 @@
         };
 
         function deleteLineItemFromCart(lineNumber_tobedeleted){
-            miniCartCtrl.baseService.startprogress();// start page level progress bar. 
-            miniCartCtrl.miniCartService.deleteLineItemFromCart(lineNumber_tobedeleted).then(function(result){
+            cartCtrl.baseService.startprogress();// start page level progress bar. 
+            cartCtrl.miniCartService.deleteLineItemFromCart(lineNumber_tobedeleted).then(function(result){
                 var retUrl = parsePagereference(result);
                 if(!_.isNull(retUrl))
                     $window.location.href = retUrl;
                 // mark minicart as dirty and reload minicart.
-                miniCartCtrl.miniCartService.setMinicartasDirty();
+                cartCtrl.miniCartService.setMinicartasDirty();
                 groupToPages();
-                miniCartCtrl.baseService.completeprogress();// stop page level progress bar.
+                cartCtrl.baseService.completeprogress();// stop page level progress bar.
             })
         };
         
-        miniCartCtrl.launch = function(which, productName, lineNumber){
+        cartCtrl.launch = function(which, productName, lineNumber){
             var dlg = null;
             switch(which){
                 // Delete Line Item Confirm Dialog
                 case 'confirmRemoveLine':
                     dlg = $dialogs.confirm('Please Confirm','Are you sure you want to Delete "'+productName+ '" from cart ?');
                     dlg.result.then(function(btn){
-                        miniCartCtrl.deleteLineItemFromCart(lineNumber);
+                        cartCtrl.deleteLineItemFromCart(lineNumber);
                     },function(btn){
                         
                 });
@@ -117,7 +117,7 @@
 
         init();
 
-        return miniCartCtrl;
+        return cartCtrl;
 	}
 
 	MiniCart.$inject = ['SystemConstants'];
@@ -137,7 +137,7 @@
 			// replace: true,
 			// transclude: true,
 			// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
-			//link: function(miniCartCtrl, iElm, iAttrs, controller) {
+			//link: function(cartCtrl, iElm, iAttrs, controller) {
 			//}
 			bindToController: true
 		};
