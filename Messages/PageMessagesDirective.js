ad236c86e4c08ac320043505124ee298e4c43c79
emmadiharish <hemmadi@apttus.com>
2015-09-09 15:05:53 -0700

    sdgsgsdf

--- Messages/PageMessagesDirective.js
+++ Messages/PageMessagesDirective.js
@@ -6,48 +6,16 @@
 
 	angular.module('APTPS_ngCPQ').directive('pageMessages', PageMessages);
 
-	PageMessageCtrl.$inject = ['$scope', '$log', 'MessageService'];
-	
-	function PageMessageCtrl($scope, $log, MessageService){
-		var messageCtrl = this;
-		messageCtrl.messages = [];
-		
-		messageCtrl.msgService = MessageService;
-
-		messageCtrl.closeMsg = function(index) {
-	        $scope.messages[index].remove();
-	    };
-
-	    $scope.$watch('messageCtrl.msgService.getMessages()', function(newVal) {
-            if(newVal)
-            {
-                messageCtrl.messages = newVal;
-            }    
-        });
-
-		messageCtrl.closeAlert = function(index) {
-			messageCtrl.msgService.removeMessage(index);
-	  	};
-
-		function init(){
-    		
-        }
-    	
-        init();
-
-        return messageCtrl;
-	}
-
-	PageMessages.$inject = ['SystemConstants'];
-	function PageMessages(SystemConstants){
+	PageMessages.$inject = ['SystemConstants', 'MessageController'];
+	function PageMessages(SystemConstants, MessageController){
 		// Runs during compile
 		return {
 			// name: '',
 			// priority: 1,
 			// terminal: true,
 			// scope: {}, // {} = isolate, true = child, false/undefined = no change
-			controller: PageMessageCtrl,
-			controllerAs: 'PageMessages',
+			controller: MessageController,
+			// controllerAs: 'PageMessages',
 			// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
 			restrict: 'AE', // E = Element, A = Attribute, C = Class, M = Comment
 			//template: '<div>pageHeader</div>',
