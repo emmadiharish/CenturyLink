1215e56558d6cbc99e6131ed7dd16b56b7141da4
emmadiharish <hemmadi@apttus.com>
2015-09-14 15:08:05 -0700

    asdasas

--- Controllers/BaseController.js
+++ Controllers/BaseController.js
@@ -302,7 +302,6 @@
 
         function formatPAVBeforeSave(pav){
             //// set the other picklist to original fields.
-
             pav = _.omit(pav, 'isDefaultLoadComplete');
             _.each(_.filter(_.keys(pav), function(pavField){
                             return pavField.endsWith('Other');
