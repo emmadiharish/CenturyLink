7566058a58e41b3e59bf9ca435569599d93e1c73
emmadiharish <harishemmadi@Harishs-MacBook-Pro.local>
2015-09-28 14:33:18 -0700

    askdnlas

---
+++ ProductAttributes/BundleAttributesDirective.js
@@ -0,0 +1,31 @@
+/**
+ * Directive: BundleAttributesDirective 
+ */
+;(function() {
+	'use strict';
+
+	angular.module('APTPS_ngCPQ').directive('bundleAttributes', BundleAttributes);
+
+	BundleAttributes.$inject = ['SystemConstants'];
+	function BundleAttributes(SystemConstants){
+		// Runs during compile
+		return {
+			// name: '',
+			// priority: 1,
+			// terminal: true,
+			// scope: {}, // {} = isolate, true = child, false/undefined = no change
+			controller: 'BundleAttributesController',
+			// controllerAs: 'BundleAttributes',
+			// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+			restrict: 'AE', // E = Element, A = Attribute, C = Class, M = Comment
+			//template: '<div>pageHeader</div>',
+			templateUrl: SystemConstants.baseUrl + "/Templates/BundleAttributesView.html",
+			// replace: true,
+			// transclude: true,
+			// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+			//link: function(cartCtrl, iElm, iAttrs, controller) {
+			//}
+			bindToController: true
+		};
+	}
+}).call(this);
