3142e53408000d2d68856265287764551da58b59
emmadiharish <harishemmadi@Harishs-MacBook-Pro.local>
2015-09-29 22:27:06 -0700

    asdlnal

---
+++ ServiceLocation/ServiceLocationDirective.js
@@ -0,0 +1,31 @@
+s/**
+ * Directive: ServiceLocationDirective 
+ */
+;(function() {
+	'use strict';
+
+	angular.module('APTPS_ngCPQ').directive('serviceLocation', ServiceLocation);
+
+	ServiceLocation.$inject = ['SystemConstants'];
+	function ServiceLocation(SystemConstants){
+		// Runs during compile
+		return {
+			// name: '',
+			// priority: 1,
+			// terminal: true,
+			// scope: {}, // {} = isolate, true = child, false/undefined = no change
+			controller: 'LocationController',
+			// controllerAs: 'ServiceLocation',
+			// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+			restrict: 'AE', // E = Element, A = Attribute, C = Class, M = Comment
+			//template: '<div>pageHeader</div>',
+			templateUrl: SystemConstants.baseUrl + "/Templates/ServiceLocationsView.html",
+			// replace: true,
+			// transclude: true,
+			// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+			//link: function(cartCtrl, iElm, iAttrs, controller) {
+			//}
+			bindToController: true
+		};
+	}
+}).call(this);
