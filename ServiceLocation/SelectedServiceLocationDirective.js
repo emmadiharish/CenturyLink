12d878ae84da56da32a8be73d58ee66f09e4bd8d
emmadiharish <hemmadi@apttus.com>
2015-10-01 13:19:37 -0700

    as;dkhal

--- ServiceLocation/SelectedServiceLocationDirective.js
+++ ServiceLocation/SelectedServiceLocationDirective.js
@@ -1,25 +1,25 @@
 /**
- * Directive: ServiceLocationsDirective 
+ * Directive: SlectedServiceLocationDirective 
  */
 ;(function() {
 	'use strict';
 
-	angular.module('APTPS_ngCPQ').directive('serviceLocations', ServiceLocations);
+	angular.module('APTPS_ngCPQ').directive('selectedServiceLocation', SelectedServiceLocation);
 
-	ServiceLocations.$inject = ['SystemConstants'];
-	function ServiceLocations(SystemConstants){
+	SelectedServiceLocation.$inject = ['SystemConstants'];
+	function SelectedServiceLocation(SystemConstants){
 		// Runs during compile
 		return {
 			// name: '',
 			// priority: 1,
 			// terminal: true,
 			// scope: {}, // {} = isolate, true = child, false/undefined = no change
-			controller: 'LocationController',
-			// controllerAs: 'ServiceLocations',
+			controller: 'SelectLocationController',
+			// controllerAs: 'SelectedServiceLocation',
 			// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
 			restrict: 'AE', // E = Element, A = Attribute, C = Class, M = Comment
 			//template: '<div>pageHeader</div>',
-			templateUrl: SystemConstants.baseUrl + "/Templates/ServiceLocationsView.html",
+			templateUrl: SystemConstants.baseUrl + "/Templates/SelectedServiceLocationView.html",
 			// replace: true,
 			// transclude: true,
 			// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
