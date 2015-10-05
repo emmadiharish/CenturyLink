e325f4e6f75f885e5a4780d1bdcc8a39bf43d5a8
emmadiharish <hemmadi@apttus.com>
2015-10-05 12:50:01 -0700

    afafdsa

---
+++ Directives/PageBodyDirective.js
@@ -0,0 +1,31 @@
+/**
+ * Directive: PageBodyDirective 
+ */
+;(function() {
+	'use strict';
+
+	angular.module('APTPS_ngCPQ').directive('PageBody', PageBody);
+
+	PageBody.$inject = ['SystemConstants'];
+	function PageBody(SystemConstants){
+		// Runs during compile
+		return {
+			// name: '',
+			// priority: 1,
+			// terminal: true,
+			// scope: {}, // {} = isolate, true = child, false/undefined = no change
+			// controller: 'BaseController',
+			// controllerAs: 'baseCon',
+			// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+			restrict: 'AE', // E = Element, A = Attribute, C = Class, M = Comment
+			//template: '<div>pageHeader</div>',
+			templateUrl: SystemConstants.baseUrl + "/Templates/PageBodyView.html",
+			// replace: true,
+			// transclude: true,
+			// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+			//link: function($scope, iElm, iAttrs, controller) {
+			//}
+			bindToController: true
+		};
+	}
+}).call(this);
