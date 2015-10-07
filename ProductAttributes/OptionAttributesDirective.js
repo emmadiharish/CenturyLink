4ab78ca3330c75243c4c4448b71e0a24b8465376
emmadiharish <harishemmadi@Harishs-MacBook-Pro.local>
2015-10-06 17:16:24 -0700

    asdlmas

--- ProductAttributes/OptionAttributesDirective.js
+++ ProductAttributes/OptionAttributesDirective.js
@@ -178,7 +178,7 @@
 			// priority: 1,
 			// terminal: true,
 			scope: {}, // {} = isolate, true = child, false/undefined = no change
-			controller: 'OptionAttributesController',
+			controller: OptionAttributesController,
 			controllerAs: 'attrCtrl',
 			// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
 			restrict: 'AE', // E = Element, A = Attribute, C = Class, M = Comment
