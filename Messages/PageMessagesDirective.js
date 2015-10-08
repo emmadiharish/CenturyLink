5ee9369547d82450344d82754ebe210e4fbc30af
emmadiharish <harishemmadi@Harishs-MacBook-Pro.local>
2015-10-08 09:17:47 -0700

    adlnal

--- Messages/PageMessagesDirective.js
+++ Messages/PageMessagesDirective.js
@@ -2,51 +2,50 @@
  * Directive: PageMessagesDirective 
  */
 ;(function() {
-	'use strict';
+            'use strict';
 
-	/*function MessageController(MessageService) {
-	    var msgCtrl = this;
+            function MessageController($scope, MessageService) {
+                
+                function init(){
+                    $scope.messages = MessageService.messages;
+                };
 
-	    function init(){
-	    	msgCtrl.messages = MessageService.messages;
-	    }
-	    
-	    msgCtrl.closeMsg = function(index) {
-	        //$scope.messages[index].remove();
-	        MessageService.removeMessage(index);
-	    };
+                $scope.closeMsg = function(index) {
+                    //$scope.messages[index].remove();
+                    MessageService.removeMessage(index);
+                };
 
-	    msgCtrl.closeAlert = function(index) {
-			MessageService.removeMessage(index);
-	  	};
+                $scope.closeAlert = function(index) {
+                    MessageService.removeMessage(index);
+                };
 
-	  	init();
-	};
+                init();
+            };
 
-	MessageController.$inject = ['MessageService'];*/
+            MessageController.$inject = ['$scope', 'MessageService'];
 
-	angular.module('APTPS_ngCPQ').directive('pageMessages', PageMessages);
+            angular.module('APTPS_ngCPQ').directive('pageMessages', PageMessages);
 
-	PageMessages.$inject = ['SystemConstants'];
-	function PageMessages(SystemConstants){
-		// Runs during compile
-		return {
-			// name: '',
-			// priority: 1,
-			// terminal: true,
-			scope: {}, // {} = isolate, true = child, false/undefined = no change
-			controller: 'MessageController',
-			// controllerAs: 'msgCtrl',
-			// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
-			restrict: 'AE', // E = Element, A = Attribute, C = Class, M = Comment
-			//template: '<div>pageHeader</div>',
-			templateUrl: SystemConstants.baseUrl + "/Templates/MessagesView.html",
-			// replace: true,
-			// transclude: true,
-			// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
-			//link: function($scope, iElm, iAttrs, controller) {
-			//}
-			bindToController: true
-		};
-	}
-}).call(this);
+            PageMessages.$inject = ['SystemConstants'];
+            function PageMessages(SystemConstants){
+                // Runs during compile
+                return {
+                    // name: '',
+                    // priority: 1,
+                    // terminal: true,
+                    scope: {}, // {} = isolate, true = child, false/undefined = no change
+                    controller: MessageController,
+                    controllerAs: 'msgCtrl',
+                    // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+                    restrict: 'AE', // E = Element, A = Attribute, C = Class, M = Comment
+                    //template: '<div>pageHeader</div>',
+                    templateUrl: SystemConstants.baseUrl + "/Templates/MessagesView.html",
+                    // replace: true,
+                    // transclude: true,
+                    // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+                    //link: function($scope, iElm, iAttrs, controller) {
+                    //}
+                    bindToController: true
+                };
+            }
+        }).call(this);
