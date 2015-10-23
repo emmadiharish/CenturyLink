1631b17ab5d130b84bf17a83466966a654a7e196
emmadiharish <hemmadi@apttus.com>
2015-10-23 14:35:34 -0700

    asdas

--- Locations/LocationDataService.js
+++ Locations/LocationDataService.js
@@ -1,73 +1,73 @@
 (function() {
-	angular.module('APTPS_ngCPQ').service('LocationDataService', LocationDataService); 
-	LocationDataService.$inject = ['$q', 'BaseService', 'BaseConfigService', 'RemoteService'];
-	function LocationDataService($q, BaseService, BaseConfigService, RemoteService) {
-		var service = this;
+    angular.module('APTPS_ngCPQ').service('LocationDataService', LocationDataService); 
+    LocationDataService.$inject = ['$q', 'BaseService', 'BaseConfigService', 'RemoteService'];
+    function LocationDataService($q, BaseService, BaseConfigService, RemoteService) {
+        var service = this;
 
-		var locationIdSet = [];
-		var isValid = false;
-		var locations = [];
-		var locIdtolocAvlsMap = {};
-		var locIdtoOptionProductsMap = {};
-		var availableOptionProducts = [];
+        var locationIdSet = [];
+        var isValid = false;
+        var locations = [];
+        var locIdtolocAvlsMap = {};
+        var locIdtoOptionProductsMap = {};
+        var availableOptionProducts = [];
 
-		var processQueue = {
-	        isRunning: [],
-	        promises: []
-	    };
+        var processQueue = {
+            isRunning: [],
+            promises: []
+        };
 
-		// location methods.
-		service.selectedlpa = {};
-		service.hasServicelocations = false;
-		service.gethasServicelocations = gethasServicelocations;
-		service.getlocItems = getlocItems;
-		service.getselectedlpa = getselectedlpa;
-		service.setselectedlpa = setselectedlpa;
-		service.getselectedlpaId = getselectedlpaId;
-		service.getalllocationIdSet = getalllocationIdSet;
-		service.getLocationAvailabilityforBundle = getLocationAvailabilityforBundle;
-		service.getLocationAvailabilityforOption = getLocationAvailabilityforOption;
-		service.getAvailableOptionProducts = getAvailableOptionProducts;
+        // location methods.
+        service.selectedlpa = {};
+        service.hasServicelocations = false;
+        service.gethasServicelocations = gethasServicelocations;
+        service.getlocItems = getlocItems;
+        service.getselectedlpa = getselectedlpa;
+        service.setselectedlpa = setselectedlpa;
+        service.getselectedlpaId = getselectedlpaId;
+        service.getalllocationIdSet = getalllocationIdSet;
+        service.getLocationAvailabilityforBundle = getLocationAvailabilityforBundle;
+        service.getLocationAvailabilityforOption = getLocationAvailabilityforOption;
+        service.getAvailableOptionProducts = getAvailableOptionProducts;
 
-		function getlocItems() {
-			if (isValid) {
-				var cachedLocations = locations;
-				// logTransaction(cachedLocations);
-				return $q.when(cachedLocations);
-			}
+        function getlocItems() {
+            if (isValid) {
+                var cachedLocations = locations;
+                // logTransaction(cachedLocations);
+                return $q.when(cachedLocations);
+            }
 
-			/*var requestPromise = RemoteService.getServiceLocations(BaseConfigService.lineItem.bundleProdId, BaseConfigService.opportunityId);
-			BaseService.startprogress();// start progress bar.
-			return requestPromise.then(function(response){
-				initializeLocations(response);
-				BaseService.setLocationLoadComplete();
-				return locations;
-			});*/
-	
-			// chain the location call and location availability calls.
-			var requestPromise = RemoteService.getServiceLocations(BaseConfigService.lineItem.bundleProdId, BaseConfigService.opportunityId);
-			BaseService.startprogress();// start progress bar.
-			var methodName = 'ServiceLocationsRequest';
+            /*var requestPromise = RemoteService.getServiceLocations(BaseConfigService.lineItem.bundleProdId, BaseConfigService.opportunityId);
+            BaseService.startprogress();// start progress bar.
+            return requestPromise.then(function(response){
+                initializeLocations(response);
+                BaseService.setLocationLoadComplete();
+                return locations;
+            });*/
+    
+            // chain the location call and location availability calls.
+            var requestPromise = RemoteService.getServiceLocations(BaseConfigService.lineItem.bundleProdId, BaseConfigService.opportunityId);
+            BaseService.startprogress();// start progress bar.
+            var methodName = 'ServiceLocationsRequest';
             var defer = $q.defer();
             if (processQueue.isRunning.indexOf(methodName) == -1) {
                 processQueue.isRunning.push(methodName);
                 requestPromise.then(function(response){
                     initializeLocations(response);
-					requestPromise = RemoteService.getLocationAvailabilities(locationIdSet, BaseConfigService.lineItem.bundleProdId);
-						return requestPromise.then(function(laresponse){
-							initializelocationAvailabilities(laresponse);
-							BaseService.setLocationLoadComplete();
-									
-						_.each(
-	                        _.filter(processQueue.promises, function (value, index) {
-	                            return value.method == methodName;
-	                        }), function (value, index) {
-	                            processQueue.promises.splice(_.indexOf(processQueue, { id: value.id }));
-	                            value.promise.resolve(locations);
-	                        });
-	                    processQueue.isRunning.splice(processQueue.isRunning.indexOf(methodName));
-					});
-				});
+                    requestPromise = RemoteService.getLocationAvailabilities(locationIdSet, BaseConfigService.lineItem.bundleProdId);
+                        return requestPromise.then(function(laresponse){
+                            initializelocationAvailabilities(laresponse);
+                            BaseService.setLocationLoadComplete();
+                                    
+                        _.each(
+                            _.filter(processQueue.promises, function (value, index) {
+                                return value.method == methodName;
+                            }), function (value, index) {
+                                processQueue.promises.splice(_.indexOf(processQueue, { id: value.id }));
+                                value.promise.resolve(locations);
+                            });
+                        processQueue.isRunning.splice(processQueue.isRunning.indexOf(methodName));
+                    });
+                });
             }
 
             processQueue.promises.push({
@@ -76,111 +76,111 @@
                 id: Date.now()
  
             });
-			return defer.promise;
-	    }
+            return defer.promise;
+        }
 
-		function initializeLocations(response) {
-			locations = response.locations;
-			isValid = true;
+        function initializeLocations(response) {
+            locations = response.locations;
+            isValid = true;
 
-			if(locations.length > 0)
-			{
-				service.hasServicelocations = true;
-				setalllocationIdSet(_.pluck(locations, 'Id'));
-			}
-		}
+            if(locations.length > 0)
+            {
+                service.hasServicelocations = true;
+                setalllocationIdSet(_.pluck(locations, 'Id'));
+            }
+        }
 
-		function initializelocationAvailabilities(response){
-			service.isValid = true;
-			_.each(response.locAvailabilities, function(la){
-				var las = [];
-				var locId = la.Service_Location__c;
-				if(_.has(locIdtolocAvlsMap, locId))
-				{
-					las = locIdtolocAvlsMap[locId];
-				}
-				las.push(la);
-				locIdtolocAvlsMap[locId] = las;
-				
-				// if option product exits then add them to locIdtoOptionProductsMap.
-				if(_.has(la, 'Option_Product__c')){
-					var optionProd = la.Option_Product__c;
-					var pIds = [];
-					if(_.has(locIdtoOptionProductsMap, locId))
-					{
-						pIds = locIdtoOptionProductsMap[locId];
-					}
-					pIds.push(optionProd);
-					locIdtoOptionProductsMap[locId] = pIds;
-				}
-			});
+        function initializelocationAvailabilities(response){
+            service.isValid = true;
+            _.each(response.locAvailabilities, function(la){
+                var las = [];
+                var locId = la.Service_Location__c;
+                if(_.has(locIdtolocAvlsMap, locId))
+                {
+                    las = locIdtolocAvlsMap[locId];
+                }
+                las.push(la);
+                locIdtolocAvlsMap[locId] = las;
+                
+                // if option product exits then add them to locIdtoOptionProductsMap.
+                if(_.has(la, 'Option_Product__c')){
+                    var optionProd = la.Option_Product__c;
+                    var pIds = [];
+                    if(_.has(locIdtoOptionProductsMap, locId))
+                    {
+                        pIds = locIdtoOptionProductsMap[locId];
+                    }
+                    pIds.push(optionProd);
+                    locIdtoOptionProductsMap[locId] = pIds;
+                }
+            });
 
-			//set selected lpa after the location availability initialization.
-			var locationId = BaseConfigService.lineItem.serviceLocationId;
+            //set selected lpa after the location availability initialization.
+            var locationId = BaseConfigService.lineItem.serviceLocationId;
             if(!_.isUndefined(locationId)
-            	&& !_.isNull(locationId))
+                && !_.isNull(locationId))
             {
                 setselectedlpa(_.findWhere(locations, {Id:locationId}));
             }
-		}
+        }
 
-		function getLocationAvailabilityforBundle(locId, productId){
-			// find the location availability record where location matches with service location and productId matches with bundle product and option product = null
-			var res = [];
-			if(_.has(locIdtolocAvlsMap, locId))
-			{
-				_.each(_.where(locIdtolocAvlsMap[locId], {Bundle_Product__c: productId}), 
-					function(la){
-					if(!_.has(la, 'Option_Product__c'))
-						res.push(la);
-				});
-			}
-			return res;
-		}
+        function getLocationAvailabilityforBundle(locId, productId){
+            // find the location availability record where location matches with service location and productId matches with bundle product and option product = null
+            var res = [];
+            if(_.has(locIdtolocAvlsMap, locId))
+            {
+                _.each(_.where(locIdtolocAvlsMap[locId], {Bundle_Product__c: productId}), 
+                    function(la){
+                    if(!_.has(la, 'Option_Product__c'))
+                        res.push(la);
+                });
+            }
+            return res;
+        }
 
-		function getLocationAvailabilityforOption(locId, productId){
-			// find the location availability record where location matches with service location and option product = productId
-			var res = [];
-			if(_.has(locIdtolocAvlsMap, locId))
-			{
-				res = _.where(locIdtolocAvlsMap[locId], {Option_Product__c: productId});
-			}
-			return res;
-		}
+        function getLocationAvailabilityforOption(locId, productId){
+            // find the location availability record where location matches with service location and option product = productId
+            var res = [];
+            if(_.has(locIdtolocAvlsMap, locId))
+            {
+                res = _.where(locIdtolocAvlsMap[locId], {Option_Product__c: productId});
+            }
+            return res;
+        }
 
-		function setAvailableOptionProductsforLocation(locId){
-			if(_.has(locIdtoOptionProductsMap, locId))
-				availableOptionProducts = locIdtoOptionProductsMap[locId];
-			else
-				availableOptionProducts = [];
-		}
+        function setAvailableOptionProductsforLocation(locId){
+            if(_.has(locIdtoOptionProductsMap, locId))
+                availableOptionProducts = locIdtoOptionProductsMap[locId];
+            else
+                availableOptionProducts = [];
+        }
 
-		function getAvailableOptionProducts(){
-			return availableOptionProducts;
-		}
+        function getAvailableOptionProducts(){
+            return availableOptionProducts;
+        }
 
-		function gethasServicelocations(){
-			return service.hasServicelocations;
-		}
-		function setselectedlpa(loc) {
-			service.selectedlpa = loc;
-			setAvailableOptionProductsforLocation(getselectedlpaId());
-		}
-		
-		function getselectedlpa() {
-			return service.selectedlpa;
-		}
+        function gethasServicelocations(){
+            return service.hasServicelocations;
+        }
+        function setselectedlpa(loc) {
+            service.selectedlpa = loc;
+            setAvailableOptionProductsforLocation(getselectedlpaId());
+        }
+        
+        function getselectedlpa() {
+            return service.selectedlpa;
+        }
 
-		function getselectedlpaId(){
-			return _.isObject(service.selectedlpa) ? service.selectedlpa.Id : '';
-		}
+        function getselectedlpaId(){
+            return _.isObject(service.selectedlpa) ? service.selectedlpa.Id : '';
+        }
 
-		function setalllocationIdSet(locIds){
-			locationIdSet = locIds;
-		}
+        function setalllocationIdSet(locIds){
+            locationIdSet = locIds;
+        }
 
-		function getalllocationIdSet(){
-			return locationIdSet;
-		}
-	}
+        function getalllocationIdSet(){
+            return locationIdSet;
+        }
+    }
 })();
--- Locations/SelectedServiceLocationDirective.js
+++ Locations/SelectedServiceLocationDirective.js
@@ -2,9 +2,9 @@
  * Directive: SlectedServiceLocationDirective 
  */
 ;(function() {
-	'use strict';
+    'use strict';
 
-	function PageMessagesController(LocationDataService) {
+    function SelectLocationController(LocationDataService) {
         
         this.getselectedlpa = function() {
             return LocationDataService.selectedlpa;
@@ -20,28 +20,28 @@
     
     SelectLocationController.$inject = ['LocationDataService'];
 
-	angular.module('APTPS_ngCPQ').directive('selectedServiceLocation', SelectedServiceLocation);
+    angular.module('APTPS_ngCPQ').directive('selectedServiceLocation', SelectedServiceLocation);
 
-	SelectedServiceLocation.$inject = ['SystemConstants'];
-	function SelectedServiceLocation(SystemConstants){
-		// Runs during compile
-		return {
-			// name: '',
-			// priority: 1,
-			// terminal: true,
-			scope: {}, // {} = isolate, true = child, false/undefined = no change
-			controller: SelectLocationController,
-			controllerAs: 'slocCtrl',
-			// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
-			restrict: 'AE', // E = Element, A = Attribute, C = Class, M = Comment
-			//template: '<div>pageHeader</div>',
-			templateUrl: SystemConstants.baseUrl + "/Templates/SelectedServiceLocationView.html",
-			// replace: true,
-			// transclude: true,
-			// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
-			//link: function(cartCtrl, iElm, iAttrs, controller) {
-			//}
-			bindToController: true
-		};
-	}
+    SelectedServiceLocation.$inject = ['SystemConstants'];
+    function SelectedServiceLocation(SystemConstants){
+        // Runs during compile
+        return {
+            // name: '',
+            // priority: 1,
+            // terminal: true,
+            scope: {}, // {} = isolate, true = child, false/undefined = no change
+            controller: SelectLocationController,
+            controllerAs: 'slocCtrl',
+            // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+            restrict: 'AE', // E = Element, A = Attribute, C = Class, M = Comment
+            //template: '<div>pageHeader</div>',
+            templateUrl: SystemConstants.baseUrl + "/Templates/SelectedServiceLocationView.html",
+            // replace: true,
+            // transclude: true,
+            // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+            //link: function(cartCtrl, iElm, iAttrs, controller) {
+            //}
+            bindToController: true
+        };
+    }
 }).call(this);
