a3ace77b4a315c8735c3d39ff1ef0af7626829c6
emmadiharish <hemmadi@apttus.com>
2015-10-23 13:15:03 -0700

    asdfasd

--- Messages/PageMessagesDirective.js
+++ Messages/PageMessagesDirective.js
@@ -2,56 +2,56 @@
  * Directive: PageMessagesDirective 
  */
 ;(function() {
-            'use strict';
+    'use strict';
 
-            function PageMessagesController(MessageService) {
-                
-                this.messages = function() {
-                    return MessageService.messages;
-                };
+    function PageMessagesController(MessageService) {
+        
+        this.messages = function() {
+            return MessageService.messages;
+        };
 
-                this.hasPageMessages = function() {
-                    return MessageService.messages.length !== 0;
-                };
+        this.hasPageMessages = function() {
+            return MessageService.messages.length !== 0;
+        };
 
-                this.closeMsg = function(index) {
-                    //$scope.messages[index].remove();
-                    MessageService.removeMessage(index);
-                };
+        this.closeMsg = function(index) {
+            //$scope.messages[index].remove();
+            MessageService.removeMessage(index);
+        };
 
-                this.closeAlert = function(index) {
-                    MessageService.removeMessage(index);
-                };
+        this.closeAlert = function(index) {
+            MessageService.removeMessage(index);
+        };
 
-                return this;
+        return this;
 
-            }
+    }
 
-            PageMessagesController.$inject = ['$scope', 
-                                                        'MessageService'];
+    PageMessagesController.$inject = ['$scope', 
+                                                'MessageService'];
 
-            angular.module('APTPS_ngCPQ').directive('pageMessages', PageMessages);
+    angular.module('APTPS_ngCPQ').directive('pageMessages', PageMessages);
 
-            PageMessages.$inject = ['SystemConstants'];
-            function PageMessages(SystemConstants){
-                // Runs during compile
-                return {
-                    // name: '',
-                    // priority: 1,
-                    // terminal: true,
-                    // scope: {}, // {} = isolate, true = child, false/undefined = no change
-                    controller: PageMessagesCController,
-                    controllerAs: 'msgCtrl',
-                    // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
-                    // restrict: 'AE', // E = Element, A = Attribute, C = Class, M = Comment
-                    //template: '<div>pageHeader</div>',
-                    templateUrl: SystemConstants.baseUrl + "/Templates/MessagesView.html",
-                    // replace: true,
-                    // transclude: true,
-                    // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
-                    //link: function($scope, iElm, iAttrs, controller) {
-                    //}
-                    bindToController: true
-                };
-            }
-        }).call(this);
+    PageMessages.$inject = ['SystemConstants'];
+    function PageMessages(SystemConstants){
+        // Runs during compile
+        return {
+            // name: '',
+            // priority: 1,
+            // terminal: true,
+            // scope: {}, // {} = isolate, true = child, false/undefined = no change
+            controller: PageMessagesController,
+            controllerAs: 'msgCtrl',
+            // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+            // restrict: 'AE', // E = Element, A = Attribute, C = Class, M = Comment
+            //template: '<div>pageHeader</div>',
+            templateUrl: SystemConstants.baseUrl + "/Templates/MessagesView.html",
+            // replace: true,
+            // transclude: true,
+            // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+            //link: function($scope, iElm, iAttrs, controller) {
+            //}
+            bindToController: true
+        };
+    }
+}).call(this);
