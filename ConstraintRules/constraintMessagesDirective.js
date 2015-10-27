febd304f29b8f03bc17e940f070359bc0b72f406
emmadiharish <harishemmadi@Harishs-MacBook-Pro.local>
2015-10-27 00:05:33 -0700

    asdhal

--- ConstraintRules/constraintMessagesDirective.js
+++ ConstraintRules/constraintMessagesDirective.js
@@ -1,91 +1,91 @@
 /**
  * Directive: ConstraintMessages
- * 	defines the directive and the controller used in the directive 
+ *  defines the directive and the controller used in the directive 
  */
 (function() {
-	'use strict';
+    'use strict';
 
-	angular.module('APTPS_ngCPQ').directive('constraintMessages', ConstraintMessages);
+    angular.module('APTPS_ngCPQ').directive('constraintMessages', ConstraintMessages);
 
-	ConstraintMessages.$inject = ['SystemConstants'];
-	function ConstraintMessages(SystemConstants) {
-		return {
-			controller: ConstraintMessagesCtrl,
-			controllerAs: 'messageCtrl',
-			bindToController: true,
-			templateUrl: SystemConstants.baseUrl + '/Templates/constraint-messages.html'
-		};
-	}
+    ConstraintMessages.$inject = ['SystemConstants'];
+    function ConstraintMessages(SystemConstants) {
+        return {
+            controller: ConstraintMessagesCtrl,
+            controllerAs: 'messageCtrl',
+            bindToController: true,
+            templateUrl: SystemConstants.baseUrl + '/Templates/constraint-messages.html'
+        };
+    }
 
-	ConstraintMessagesCtrl.$inject = [
-		'ConstraintRuleDataService'
-	];
-	
-	function ConstraintMessagesCtrl(ConstraintRuleDataService) {
-		var ctrl = this;
-		
-		// ctrl.labels = i18nService.CustomLabel;
-		ctrl.errorMessages = [];
-		ctrl.messageField = 'Message';
+    ConstraintMessagesCtrl.$inject = [
+        'ConstraintRuleDataService'
+    ];
+    
+    function ConstraintMessagesCtrl(ConstraintRuleDataService) {
+        var ctrl = this;
+        
+        // ctrl.labels = i18nService.CustomLabel;
+        ctrl.errorMessages = [];
+        ctrl.messageField = 'Message';
 
-		ctrl.pageErrors = function() {
-			return ctrl.messenger.getMessages().page.error;
-		};
+        ctrl.pageErrors = function() {
+            return ctrl.messenger.getMessages().page.error;
+        };
 
-		ctrl.pageWarnings = function() {
-			return ctrl.messenger.getMessages().page.warning;
-		};
+        ctrl.pageWarnings = function() {
+            return ctrl.messenger.getMessages().page.warning;
+        };
 
-		function processCommonErrors() {
-			ctrl.errorMessages.length = 0;
+        function processCommonErrors() {
+            ctrl.errorMessages.length = 0;
 
-			var contextBundleNumber = ConstraintRuleDataService.getContextBundleNumber();
-			var errorLineNumbers = ctrl.messenger.getCommonErrorLines();
+            var contextBundleNumber = ConstraintRuleDataService.getContextBundleNumber();
+            var errorLineNumbers = ctrl.messenger.getCommonErrorLines();
 
-			/*if (errorLineNumbers.length !== 0) {
-				var lineItemsWithErrors = OptionGroupDataService.getLineItems(errorLineNumbers);
-				angular.forEach(lineItemsWithErrors, function(nextLineItem, index) {
-					var lineSequence = nextLineItem.primaryLine().sequence();
-					var productName = nextLineItem.productName();
-					var newError = {
-						message: 'Configuration Pending for: '+productName;
-					}; 
-					ctrl.errorMessages.push(newError);
-				});
-			}*/
-			return ctrl.errorMessages;
-		}
+            /*if (errorLineNumbers.length !== 0) {
+                var lineItemsWithErrors = OptionGroupDataService.getLineItems(errorLineNumbers);
+                angular.forEach(lineItemsWithErrors, function(nextLineItem, index) {
+                    var lineSequence = nextLineItem.primaryLine().sequence();
+                    var productName = nextLineItem.productName();
+                    var newError = {
+                        message: 'Configuration Pending for: '+productName;
+                    }; 
+                    ctrl.errorMessages.push(newError);
+                });
+            }*/
+            return ctrl.errorMessages;
+        }
 
-		ctrl.pageInfos = function() {
-			return ctrl.messenger.getMessages().page.info;
-		};
+        ctrl.pageInfos = function() {
+            return ctrl.messenger.getMessages().page.info;
+        };
 
-		ctrl.hasError = function() {
-			return ctrl.messenger.getMessages().page.error.length !== 0;
-		};
+        ctrl.hasError = function() {
+            return ctrl.messenger.getMessages().page.error.length !== 0;
+        };
 
-		ctrl.hasWarning = function() {
-			return ctrl.messenger.getMessages().page.warning.length !== 0;
-		};
+        ctrl.hasWarning = function() {
+            return ctrl.messenger.getMessages().page.warning.length !== 0;
+        };
 
-		ctrl.hasCommonErrors = function() {
-			return ctrl.messenger.getCommonErrorLines().length !== 0;
-		};
+        ctrl.hasCommonErrors = function() {
+            return ctrl.messenger.getCommonErrorLines().length !== 0;
+        };
 
-		ctrl.hasInfo = function() {
-			return ctrl.messenger.getMessages().page.info.length !== 0;
-		};
+        ctrl.hasInfo = function() {
+            return ctrl.messenger.getMessages().page.info.length !== 0;
+        };
 
-		ctrl.hasMessages = function() {
-			return ctrl.hasError() || ctrl.hasWarning() || ctrl.hasInfo() || ctrl.hasCommonErrors();
-		};
+        ctrl.hasMessages = function() {
+            return ctrl.hasError() || ctrl.hasWarning() || ctrl.hasInfo() || ctrl.hasCommonErrors();
+        };
 
-		ctrl.messenger = ConstraintRuleDataService;
+        ctrl.messenger = ConstraintRuleDataService;
 
-		//initialize
-		processCommonErrors();
+        //initialize
+        processCommonErrors();
 
-		return ctrl;
-	}
+        return ctrl;
+    }
 
 }).call(this);
