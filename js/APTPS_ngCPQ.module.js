7fcf9c97990508c00fcddbd4a9c14597b849cd5f
emmadiharish <hemmadi@apttus.com>
2015-11-09 14:40:31 -0800

    asdkab

--- js/APTPS_ngCPQ.module.js
+++ js/APTPS_ngCPQ.module.js
@@ -14,20 +14,7 @@
 		'ui.select',
 		'dirPagination']
 		).constant()
-			.config(configBlock);
-
-	configBlock.$inject = [
-		'systemConstants',
-		'paginationTemplateProvider'
-	];
-	
-	function configBlock(systemConstants, paginationTemplateProvider) {
-		var baseUrl = systemConstants.baseUrl;
-
-		//A single pagination-controls template is used throught the app. This may be limiting.
-		paginationTemplateProvider.setPath(baseUrl + '/Templates/pagination.html');
-
-	}
+			.config();
 }).call(this);
 
 // angular.module('APTPS_ngCPQ', ['ngProgress']);
