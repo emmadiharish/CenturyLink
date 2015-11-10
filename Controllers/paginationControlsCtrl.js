470ad28f0efff78c777920a9a2c8b111383b7a67
emmadiharish <harishemmadi@Harishs-MacBook-Pro.local>
2015-11-09 16:39:20 -0800

    abs

---
+++ Controllers/paginationControlsCtrl.js
@@ -0,0 +1,11 @@
+(function() {
+    'use strict';
+    
+	var PaginationControlsCtrl;
+
+	PaginationControlsCtrl = function(){
+	    var paginationCtrl = this;
+	}
+
+	angular.module('APTPS_ngCPQ').controller('paginationControlsCtrl', PaginationControlsCtrl);
+}).call(this);
